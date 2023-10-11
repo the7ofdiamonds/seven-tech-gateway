eb3d14c4f74b14eb76bd29c1a1f613e45be1abd3
Jamel Lyons <32249473+the7ofdiamonds@users.noreply.github.com>
2023-10-11 10:14:08 -0400

    Working on the frontend css only loads with plugin

--- css/CSS.php
+++ css/CSS.php
@@ -1,20 +1,24 @@
 <?php
+
 namespace THFW_Users\CSS;
 
 use THFW\CSS\Customizer\Customizer;
 
-class CSS {
-    
-    public function __construct() {
+class CSS
+{
+
+    public function __construct()
+    {
         add_action('wp_head', [$this, 'load_css']);
 
         // new Customizer;
     }
 
-    //Load Plugin CSS & JS
-    function load_css(){
-        
-        wp_register_style('thfw_users_css',  THFW_USERS_URL . 'CSS/thfw-users.css', array(), false, 'all' );
-        wp_enqueue_style('thfw_users_css');
-    }  
+    function load_css()
+    {
+        if (is_front_page() || is_page('about') || is_page('login') || is_page('signup') || is_page('forgot') || is_page('logout') || is_page('dashboard') || is_post_type_archive('team') || is_singular('team')) {
+            wp_register_style('thfw_users_css',  THFW_USERS_URL . 'CSS/thfw-users.css', array(), false, 'all');
+            wp_enqueue_style('thfw_users_css');
+        }
+    }
 }
