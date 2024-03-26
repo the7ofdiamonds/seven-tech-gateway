c939eeaa2237907d0cffc94af0b90b746beac362
Jamel Lyons <32249473+the7ofdiamonds@users.noreply.github.com>
2024-03-26 13:23:41 -0400

    Refactoring 3/26/24 @ 13:16

--- dist/js/Team.js
+++
@@ -1,2 +0,0 @@
-import{u as n,r as i,af as u,j as r,ac as c}from"./index.js";import{E as f}from"./ErrorComponent.js";import{G as p}from"./GroupMembers.js";import{u as x}from"./useDispatch.js";function g(){var e=n(function(m){return m.team}),s=e.teamLoading,t=e.teamError,o=e.team,a=x();return i.useEffect(function(){a(u())},[a]),s?r.jsx(c,{}):t?r.jsx(f,{error:t}):r.jsx(r.Fragment,{children:r.jsxs("section",{className:"team",children:[r.jsx("h4",{className:"title",children:"Team"}),r.jsx(p,{group:o})]})})}export{g as default};
-//# sourceMappingURL=Team.js.map
