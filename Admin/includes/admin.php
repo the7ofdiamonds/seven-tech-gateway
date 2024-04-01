4fa1aa4900e3f43bd6e937a329f724beb7a88040
Jamel Lyons <32249473+the7ofdiamonds@users.noreply.github.com>
2024-04-01 15:08:38 -0400

    Refactoring 4/1/24 @ 15:08

--- Admin/includes/admin.php
+++
@@ -1,9 +0,0 @@
-<section class="seven-tech-admin">
-    <h1>Settings</h1>
-    <?php settings_errors(); ?>
-    <form method="post" action="options.php">
-        <?php settings_fields('seven-tech-admin-group'); ?>
-        <?php do_settings_sections('seven_tech_admin'); ?>
-        <?php submit_button(); ?>
-    </form>
-</section>
--- dist/js/ErrorComponent.js
+++
@@ -1,2 +0,0 @@
-import{j as r}from"./index.js";function o(s){var e=s.error;return r.jsx("main",{className:"error",children:r.jsx("div",{className:"status-bar card error",children:r.jsx("span",{children:e})})})}export{o as E};
-//# sourceMappingURL=ErrorComponent.js.map
