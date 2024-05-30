42f32693a3a7ac8861430a635fc4b85f65d05c94
jamelclyons <jamel.c.lyons@gmail.com>
2024-05-30 16:26:14 -0400

    Refactoring 5/30/24 @ 16:26

--- Account/CreateAccount.php
+++ Account/CreateAccount.php
@@ -3,6 +3,7 @@
 namespace SEVEN_TECH\Gateway\Account;
 
 use SEVEN_TECH\Gateway\Exception\DestructuredException;
+use SEVEN_TECH\Gateway\Roles\Roles;
 use SEVEN_TECH\Gateway\Validator\Validator;
 
 use Exception;
@@ -12,19 +13,90 @@ use Kreait\Firebase\Contract\Auth;
 class CreateAccount
 {
     private $auth;
+    private $roles;
 
     public function __construct(Auth $auth)
     {
         $this->auth = $auth;
+        $this->roles = new Roles;
     }
 
-    // Search for email
+    function existsByEmail($email)
+    {
+        global $wpdb;
 
-    // Search for username
+        $results = $wpdb->get_results(
+            $wpdb->prepare("CALL existsByEmail('%s')", $email)
+        );
 
-    // Search for nicename
+        if ($wpdb->last_error) {
+            throw new Exception("Error executing stored procedure: " . $wpdb->last_error, 500);
+        }
+
+        if (!isset($results[0])) {
+            throw new Exception('There was an error searching for Account.', 404);
+        }
+
+        return $results[0]->resultSet;
+    }
+
+    function existsByUsername($username)
+    {
+        global $wpdb;
+
+        $results = $wpdb->get_results(
+            $wpdb->prepare("CALL existsByUsername('%s')", $username)
+        );
+
+        if ($wpdb->last_error) {
+            throw new Exception("Error executing stored procedure: " . $wpdb->last_error, 500);
+        }
+
+        if (!isset($results[0])) {
+            throw new Exception('There was an error searching for Account.', 404);
+        }
+
+        return $results[0]->resultSet;
+    }
+
+    function existsByNicename($nicename)
+    {
+        global $wpdb;
+
+        $results = $wpdb->get_results(
+            $wpdb->prepare("CALL existsByNicename('%s')", $nicename)
+        );
+
+        if ($wpdb->last_error) {
+            throw new Exception("Error executing stored procedure: " . $wpdb->last_error, 500);
+        }
+
+        if (!isset($results[0])) {
+            throw new Exception('There was an error searching for Account.', 404);
+        }
+
+        return $results[0]->resultSet;
+    }
 
     // Search for phone
+    function existsByPhone($phone)
+    {
+        global $wpdb;
+
+        $results = $wpdb->get_results(
+            $wpdb->prepare("CALL existsByPhone('%s')", $phone)
+        );
+
+        if ($wpdb->last_error) {
+            throw new Exception("Error executing stored procedure: " . $wpdb->last_error, 500);
+        }
+
+        if (!isset($results[0])) {
+            throw new Exception('There was an error searching for Account.', 404);
+        }
+
+        return $results[0]->resultSet;
+    }
 
     // Check password
     function checkPassword($password)
@@ -54,24 +126,46 @@ class CreateAccount
     function createAccount($email, $username, $password, $nicename, $nickname, $firstname, $lastname, $phone, $roles)
     {
         try {
+
+            $emailExists = $this->existsByEmail($email);
+
+            if ($emailExists == 'TRUE') {
+                throw new Exception('This email is currently in use check your inbox.', 400);
+            }
+
+            $usernameExists = $this->existsByUsername($username);
+
+            if ($usernameExists == 'TRUE') {
+                throw new Exception('This username is in use at this time.', 400);
+            }
+
+            $nicenameExists = $this->existsByNicename($nicename);
+
+            if ($nicenameExists == 'TRUE') {
+                throw new Exception('This nicename is in use at this time.', 400);
+            }
+
+            $phoneExists = $this->existsByPhone($phone);
+
+            if ($phoneExists == 'TRUE') {
+                throw new Exception('This phone number is in use at this time.', 400);
+            }
+
             $newFirebaseUser = $this->createFirebaseUser($email, $phone, $password, $username);
             $providergivenID = $newFirebaseUser->uid;
-
+            error_log($providergivenID);
             if (empty($providergivenID)) {
                 error_log("Unable to add user with email {$email} to firebase.");
             }
 
             $hashedPassword = $this->checkPassword($password);
 
-            $role = array('subscriber');
-            $roles = serialize($role);
-
             $user_activation_key = wp_generate_password(20, false);
 
             global $wpdb;
 
             $results = $wpdb->get_results(
-                $wpdb->prepare("CALL addNewUser('%s', '%s','%s','%s','%s','%s','%s','%s','%s','%s')", $email, $username, $hashedPassword, $nicename, $nickname, $firstname, $lastname, $phone, $roles, $user_activation_key)
+                $wpdb->prepare("CALL createAccount('%s', '%s','%s','%s','%s','%s','%s','%s','%s')", $email, $username, $hashedPassword, $nicename, $nickname, $firstname, $lastname, $phone, $user_activation_key)
             );
 
             if ($wpdb->last_error) {
@@ -81,11 +175,18 @@ class CreateAccount
             $account = $results[0];
 
             if (empty($account)) {
-                throw new Exception("User could not be added.", 404);
+                throw new Exception("Account could not be created.", 404);
             }
 
+            if (empty($roles)) {
+                $updatedAccountID = $this->roles->addRole($account->id, 'subscriber', 'Subscriber');
+            }
+
+            if ($account->id !== $updatedAccountID) {
+                throw new Exception('The wrong account may have been updated check records.');
+            }
             // send signup email with activation code
-            return $account;
+            return new Account($account->email);
         } catch (DestructuredException $e) {
             throw new DestructuredException($e);
         } catch (Exception $e) {
--- Roles/Roles.php
+++ Roles/Roles.php
@@ -2,8 +2,110 @@
 
 namespace SEVEN_TECH\Gateway\Roles;
 
+use SEVEN_TECH\Gateway\Exception\DestructuredException;
+
+use Exception;
+
+use WP_Error;
+use WP_User;
+
 class Roles
 {
+    public function roleExists($roleName, $roleDisplayName)
+    {
+        $wp_roles = wp_roles()->get_names();
+
+        $roleExists = false;
+
+        foreach ($wp_roles as $roleKey => $roleValue) {
+            if ($roleKey == $roleName && $roleValue == $roleDisplayName) {
+                $roleExists = true;
+                break;
+            }
+        }
+
+        return $roleExists;
+    }
+
+    public function addRole($id, $roleName, $roleDisplayName)
+    {
+        try {
+            if (empty($id)) {
+                throw new Exception('ID is required to add role.');
+            }
+
+            if (empty($roleName)) {
+                throw new Exception('Role name is required to add role.');
+            }
+
+            if (empty($roleDisplayName)) {
+                throw new Exception('Role display name is required to add role.');
+            }
+
+            $roleExists = $this->roleExists($roleName, $roleDisplayName);
+
+            if ($roleExists == false) {
+                throw new Exception("Role {$roleDisplayName} does not exits.", 404);
+            }
+
+            $user = new WP_User($id);
+            $user->add_role($roleName);
+
+            $updated = wp_update_user($user);
+
+            if (!is_int($updated)) {
+                throw new Exception("There has been an error adding user role.", 500);
+            }
+
+            return $updated;
+        } catch (WP_Error $e) {
+            throw new DestructuredException($e);
+        } catch (DestructuredException $e) {
+            throw new DestructuredException($e);
+        } catch (Exception $e) {
+            throw new DestructuredException($e);
+        }
+    }
+
+    public function removeRole($id, $roleName)
+    {
+        try {
+            if (empty($id)) {
+                throw new Exception('ID is required to remove role.');
+            }
+
+            if (empty($roleName)) {
+                throw new Exception('Role name is required to remove role.');
+            }
+
+            $user = new WP_User($id);
+            $user_roles = $user->roles;
+
+            $hasRole = false;
+
+            foreach ($user_roles as $user_role) {
+                if ($roleName == $user_role) {
+                    $hasRole = true;
+                }
+            }
+
+            if ($hasRole) {
+                $user->remove_role($roleName);
+            }
+
+            $updated = wp_update_user($user);
+
+            if (!is_int($updated)) {
+                return "There has been an error removing user role.";
+            }
+
+            return "User role {$roleName} has been removed successfully";
+        } catch (DestructuredException $e) {
+            throw new DestructuredException($e);
+        } catch (Exception $e) {
+            throw new DestructuredException($e);
+        }
+    }
 
     public function getRoles()
     {
@@ -21,22 +123,6 @@ class Roles
         return $roles;
     }
 
-    public function roleExists($roleName, $roleDisplayName)
-    {
-        $wp_roles = wp_roles()->get_names();
-
-        $roleExists = false;
-
-        foreach ($wp_roles as $roleKey => $roleValue) {
-            if ($roleKey == $roleName && $roleValue == $roleDisplayName) {
-                $roleExists = true;
-                break;
-            }
-        }
-
-        return $roleExists;
-    }
-
     public function unserializeRoles($serializedRoles)
     {
         if (empty($serializedRoles)) {
@@ -47,10 +133,10 @@ class Roles
 
         $uroles = [];
 
-        if(!is_array($unserializedRoles)){
+        if (!is_array($unserializedRoles)) {
             $uroles[] = $unserializedRoles;
         } else {
-            $uroles = $unserializedRoles; 
+            $uroles = $unserializedRoles;
         }
 
         $wp_roles = wp_roles()->get_names();
