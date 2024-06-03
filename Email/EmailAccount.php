8ee8843b1692c75bdddb0a8aca04828a91c71fbc
jamelclyons <jamel.c.lyons@gmail.com>
2024-06-03 13:00:50 -0400

    Refactoring 6/3/24 @ 13:00

--- Email/EmailAccount.php
+++ Email/EmailAccount.php
@@ -2,46 +2,192 @@
 
 namespace SEVEN_TECH\Gateway\Email;
 
-use Exception;
-
+use SEVEN_TECH\Gateway\Account\Account;
 use SEVEN_TECH\Gateway\Database\DatabaseExists;
 use SEVEN_TECH\Gateway\Exception\DestructuredException;
-use SEVEN_TECH\Gateway\Validator\Validator;
 
 class EmailAccount
 {
-    private $validator;
     private $exists;
-    private $body;
+    private $email;
 
     public function __construct()
     {
-        $this->validator = new Validator;
         $this->exists = new DatabaseExists;
-        $this->body = SEVEN_TECH_GATEWAY . 'Templates/TemplatesEmailGatewayLink.php';
+        $this->email = new Email;
     }
 
-    function accountCreated()
+    function accountCreated($email)
     {
+        try {
+            $this->exists->existsByEmail($email);
+
+            $account = new Account($email);
+
+            $subject = "Your new account at {$this->email->site_name}";
+// Create a welcome email template
+            $template = SEVEN_TECH_GATEWAY . 'Templates/TemplatesEmailBody.php';
+
+            $message = "Your password has been changed.";
+            $url = home_url() . "/login";
+            $button_name = 'LOGIN';
+
+            $content = array(
+                "{MESSAGE}" => $message,
+                "{URL}" => $url,
+                "{BUTTON_NAME}" => $button_name
+            );
+
+            $this->email->sendEmail($account, $subject, $template, $content, $message);
+
+            return "Your password has been changed an email has been sent to {$email} check your inbox to confirm this action was completed.";
+        } catch (DestructuredException $e) {
+            throw new DestructuredException($e);
+        }
     }
 
-    function accountLocked()
+    function accountLocked($email)
     {
+        try {
+            $this->exists->existsByEmail($email);
+
+            $account = new Account($email);
+
+            $subject = "Your account has been locked at {$this->email->site_name}";
+
+            $template = SEVEN_TECH_GATEWAY . 'Templates/TemplatesEmailBody.php';
+
+            $message = "Your account has been locked.";
+            $url = home_url() . "/login";
+            $button_name = 'LOGIN';
+
+            $content = array(
+                "{MESSAGE}" => $message,
+                "{URL}" => $url,
+                "{BUTTON_NAME}" => $button_name
+            );
+
+            $this->email->sendEmail($account, $subject, $template, $content, $message);
+
+            return "Your account has been locked.";
+        } catch (DestructuredException $e) {
+            throw new DestructuredException($e);
+        }
     }
 
-    function accountUnlocked()
+    function accountUnlocked($email)
     {
+        try {
+            $this->exists->existsByEmail($email);
+
+            $account = new Account($email);
+
+            $subject = "Your account has been unlocked at {$this->email->site_name}";
+
+            $template = SEVEN_TECH_GATEWAY . 'Templates/TemplatesEmailBody.php';
+
+            $message = "Your account has been unlocked.";
+            $url = home_url() . "/login";
+            $button_name = 'LOGIN';
+
+            $content = array(
+                "{MESSAGE}" => $message,
+                "{URL}" => $url,
+                "{BUTTON_NAME}" => $button_name
+            );
+
+            $this->email->sendEmail($account, $subject, $template, $content, $message);
+
+            return "Your password has been changed an email has been sent to {$email} check your inbox to confirm this action was completed.";
+        } catch (DestructuredException $e) {
+            throw new DestructuredException($e);
+        }
     }
 
-    function accountDisabled()
+    function accountDisabled($email)
     {
+        try {
+            $this->exists->existsByEmail($email);
+
+            $account = new Account($email);
+
+            $subject = "Your account has been disabled at {$this->email->site_name}";
+
+            $template = SEVEN_TECH_GATEWAY . 'Templates/TemplatesEmailBody.php';
+
+            $message = "Your account has been disabled.";
+            $url = home_url() . "/login";
+            $button_name = 'LOGIN';
+
+            $content = array(
+                "{MESSAGE}" => $message,
+                "{URL}" => $url,
+                "{BUTTON_NAME}" => $button_name
+            );
+
+            $this->email->sendEmail($account, $subject, $template, $content, $message);
+
+            return "Your account has been disabled.";
+        } catch (DestructuredException $e) {
+            throw new DestructuredException($e);
+        }
     }
 
-    function accountEnabled()
+    function accountEnabled($email)
     {
+        try {
+            $this->exists->existsByEmail($email);
+
+            $account = new Account($email);
+
+            $subject = "Your account has been enabled at {$this->email->site_name}";
+// Create a welcome email template
+            $template = SEVEN_TECH_GATEWAY . 'Templates/TemplatesEmailBody.php';
+
+            $message = "Your password has been changed.";
+            $url = home_url() . "/login";
+            $button_name = 'LOGIN';
+
+            $content = array(
+                "{MESSAGE}" => $message,
+                "{URL}" => $url,
+                "{BUTTON_NAME}" => $button_name
+            );
+
+            $this->email->sendEmail($account, $subject, $template, $content, $message);
+
+            return "Your account has been enabled.";
+        } catch (DestructuredException $e) {
+            throw new DestructuredException($e);
+        }
     }
 
-    function accountDeleted()
+    function accountDeleted($email)
     {
+        try {
+            $this->exists->existsByEmail($email);
+
+            $account = new Account($email);
+
+            $subject = "Your account at {$this->email->site_name} has been deleted.";
+
+            $template = SEVEN_TECH_GATEWAY . 'Templates/TemplatesEmailBody.php';
+
+            $message = "Your password has been changed.";
+            $url = home_url() . "/login";
+            $button_name = 'LOGIN';
+
+            $content = array(
+                "{MESSAGE}" => $message,
+                "{URL}" => $url,
+                "{BUTTON_NAME}" => $button_name
+            );
+
+            $this->email->sendEmail($account, $subject, $template, $content, $message);
+
+            return "This account has been deleted.";
+        } catch (DestructuredException $e) {
+            throw new DestructuredException($e);
+        }
     }
 }
